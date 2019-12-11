e48a53fd3c7528bdaf8e4c9b023810f14ace4fa7
Andrey Grachev <andreysgra@gmail.com>
2019-12-11 22:11:07 +0300

    Добавляет заглушку отсутствия точек маршрута.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import TripDayComponent from './components/trip-day.js';
 import TripEventsComponent from './components/trip-events.js';
 import TripEventComponent from './components/trip-event.js';
 import TripEventEditComponent from './components/trip-event-edit.js';
+import NoEventsComponent from './components/no-events.js';
 import {renderComponent, replaceElement, addEscapeEvent} from './utils.js';
 import {FILTERS, MENU_ITEMS, RenderPosition} from './const.js';
 import {generateEvents} from './mock/event.js';
@@ -64,30 +65,36 @@ const renderEvent = (container, event) => {
   renderComponent(container.getElement(), eventComponent.getElement());
 };
 
-const tripInfo = document.querySelector(`.trip-main__trip-info`);
-renderComponent(tripInfo, new TripInfoComponent(events).getElement(), RenderPosition.AFTERBEGIN);
-
 const tripControls = document.querySelector(`.trip-main__trip-controls`);
+const tripInfo = document.querySelector(`.trip-main__trip-info`);
+
 renderComponent(tripControls, new MenuComponent(MENU_ITEMS).getElement(), RenderPosition.AFTERBEGIN);
 renderComponent(tripControls, new FilterComponent(FILTERS).getElement());
 
 const tripEvents = document.querySelector(`.trip-events`);
-renderComponent(tripEvents, new TripSortComponent().getElement());
 
-const tripDaysComponent = new TripDaysComponent();
-renderComponent(tripEvents, tripDaysComponent.getElement());
+if (events.length === 0) {
+  renderComponent(tripEvents, new NoEventsComponent().getElement());
+} else {
+  renderComponent(tripInfo, new TripInfoComponent(events).getElement(), RenderPosition.AFTERBEGIN);
 
-dates.forEach((date, index) => {
-  const tripDayComponent = new TripDayComponent(date, index + 1);
-  renderComponent(tripDaysComponent.getElement(), tripDayComponent.getElement());
+  renderComponent(tripEvents, new TripSortComponent().getElement());
 
-  const tripEventsComponent = new TripEventsComponent();
-  renderComponent(tripDayComponent.getElement(), tripEventsComponent.getElement());
+  const tripDaysComponent = new TripDaysComponent();
+  renderComponent(tripEvents, tripDaysComponent.getElement());
 
-  events
-    .filter((event) => new Date(event.startDate).toDateString() === date)
-    .forEach((event) => renderEvent(tripEventsComponent, event));
-});
+  dates.forEach((date, index) => {
+    const tripDayComponent = new TripDayComponent(date, index + 1);
+    renderComponent(tripDaysComponent.getElement(), tripDayComponent.getElement());
 
-const tripCost = events.reduce((acc, value) => acc + value.price, 0);
-tripInfo.querySelector(`.trip-info__cost-value`).textContent = tripCost;
+    const tripEventsComponent = new TripEventsComponent();
+    renderComponent(tripDayComponent.getElement(), tripEventsComponent.getElement());
+
+    events
+      .filter((event) => new Date(event.startDate).toDateString() === date)
+      .forEach((event) => renderEvent(tripEventsComponent, event));
+  });
+
+  const tripCost = events.reduce((acc, value) => acc + value.price, 0);
+  tripInfo.querySelector(`.trip-info__cost-value`).textContent = tripCost;
+}
