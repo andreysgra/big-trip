923d93304962f6625c60ee26655237e68a53f20a
Andrey Grachev <andreysgra@gmail.com>
2020-01-16 20:01:25 +0300

    Удаляет из проекта неиспользуемые функции.

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,23 +1,5 @@
 import {EventType} from '../const';
 
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-export const getRandomArrayItem = (array) => array[getRandomNumber(0, array.length)];
-
-export const getRandomBool = () => Math.random() > 0.5;
-
-export const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let random = Math.floor(Math.random() * (i + 1));
-    let temp = array[random];
-
-    array[random] = array[i];
-    array[i] = temp;
-  }
-
-  return array;
-};
-
 export const toUpperCaseFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 export const formatEventTypePlaceholder = (eventType) => {
