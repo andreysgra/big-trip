e0e95ac5f332154ad78d4e37486713cefe168133
andreysgra <andreysgra@users.noreply.github.com>
2021-01-11 08:55:06 +0000

    deploy: d403e3611bb8ea849fde89fadc3c09114c70a392

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = `bigtrip-cache`;
-const CACHE_VER = `v1`;
+const CACHE_VER = `v10`;
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
 
 const installHandler = (evt) => {
@@ -7,23 +7,23 @@ const installHandler = (evt) => {
       caches.open(CACHE_NAME)
         .then((cache) => {
           return cache.addAll([
-            `/`,
-            `/index.html`,
-            `/bundle.js`,
-            `/css/style.css`,
-            `/img/icons/bus.png`,
-            `/img/icons/check-in.png`,
-            `/img/icons/drive.png`,
-            `/img/icons/flight.png`,
-            `/img/icons/restaurant.png`,
-            `/img/icons/ship.png`,
-            `/img/icons/sightseeing.png`,
-            `/img/icons/taxi.png`,
-            `/img/icons/train.png`,
-            `/img/icons/transport.png`,
-            `/img/header-bg.png`,
-            `/img/header-bg@2x.png`,
-            `/img/logo.png`
+            `./`,
+            `./index.html`,
+            `./bundle.js`,
+            `./css/style.css`,
+            `./img/icons/bus.png`,
+            `./img/icons/check-in.png`,
+            `./img/icons/drive.png`,
+            `./img/icons/flight.png`,
+            `./img/icons/restaurant.png`,
+            `./img/icons/ship.png`,
+            `./img/icons/sightseeing.png`,
+            `./img/icons/taxi.png`,
+            `./img/icons/train.png`,
+            `./img/icons/transport.png`,
+            `./img/header-bg.png`,
+            `./img/header-bg@2x.png`,
+            `./img/logo.png`
           ]);
         })
   );
