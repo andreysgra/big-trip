c5d8804573e1e5df253af75224ea5622a5a48ea0
Andrey Grachev <andreysgra@gmail.com>
2021-01-11 15:13:10 +0300

    Вносит исправления.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require(`path`);
-const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
-const TerserPlugin = require(`terser-webpack-plugin`);
+const path = require('path');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const isProd = Boolean(process.env.NODE_ENV);
 
 module.exports = {
-  mode: isProd ? `production` : `development`,
-  entry: `./src/main.js`,
+  mode: isProd ? 'production' : 'development',
+  entry: './src/main.js',
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
   },
-  devtool: isProd ? false : `source-map`,
+  devtool: isProd ? false : 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     watchContentBase: true
   },
@@ -21,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [`style-loader`, `css-loader`],
+        use: ['style-loader', 'css-loader'],
       }
     ]
   },
