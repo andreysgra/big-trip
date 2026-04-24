6a112677cb8935a74f44e9714e16370e8b755c69
Andrey Grachev <andreysgra@gmail.com>
2026-04-24 17:28:18 +0300

    Создаёт модули компонентов.

---
+++ src/view/trip-info-view.js
@@ -0,0 +1,11 @@
+import AbstractView from '../framework/view/abstract-view';
+
+const createTripInfoTemplate = () => (
+  '<section class="trip-main__trip-info trip-info"></section>'
+);
+
+export default class TripInfoView extends AbstractView {
+  get template() {
+    return createTripInfoTemplate();
+  }
+}
