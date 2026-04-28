72adef64e2781599076a7c52afde342ea92915ae
Andrey Grachev <andreysgra@gmail.com>
2026-04-28 13:26:17 +0300

    Создаёт тестовые данные.

---
+++ src/mocks/destinations.js
@@ -0,0 +1,156 @@
+export const destinations = [
+  {
+    'id': '84da8247-0d64-448b-b8b5-e9448741ebfd',
+    'description': 'Venice - in a middle of Europe',
+    'name': 'Venice',
+    'pictures': []
+  },
+  {
+    'id': '434f6f8f-5d2c-4127-91be-fcc20c092f4f',
+    'description': '',
+    'name': 'Sochi',
+    'pictures': []
+  },
+  {
+    'id': 'ee0f1753-76ce-4d45-9177-f416aeaf356f',
+    'description': 'Berlin - famous for its crowded street markets with the best street food in Asia',
+    'name': 'Berlin',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/11.jpg',
+        'description': 'Berlin famous for its crowded street markets with the best street food in Asia'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/13.jpg',
+        'description': 'Berlin with a beautiful old town'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/13.jpg',
+        'description': 'Berlin a true asian pearl'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/19.jpg',
+        'description': 'Berlin with a beautiful old town'
+      }
+    ]
+  },
+  {
+    'id': 'fb31ebb9-0dc0-4270-a31b-cd0cc95d84d2',
+    'description': 'Hiroshima - is a beautiful city',
+    'name': 'Hiroshima',
+    'pictures': []
+  },
+  {
+    'id': '5d35b943-f7b9-4bbf-aa20-1d3f49c2479d',
+    'description': 'Geneva - with a beautiful old town',
+    'name': 'Geneva',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/18.jpg',
+        'description': 'Geneva is a beautiful city'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/6.jpg',
+        'description': 'Geneva famous for its crowded street markets with the best street food in Asia'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/13.jpg',
+        'description': 'Geneva with a beautiful old town'
+      }
+    ]
+  },
+  {
+    'id': 'd501cb2e-7c24-4ffb-a8a6-15c7bcb72d1f',
+    'description': 'Barcelona - a perfect place to stay with a family',
+    'name': 'Barcelona',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/17.jpg',
+        'description': 'Barcelona a perfect place to stay with a family'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/8.jpg',
+        'description': 'Barcelona with crowded streets'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/12.jpg',
+        'description': 'Barcelona is a beautiful city'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/20.jpg',
+        'description': 'Barcelona is a beautiful city'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/19.jpg',
+        'description': 'Barcelona is a beautiful city'
+      }
+    ]
+  },
+  {
+    'id': '708e05ca-a0ea-45b2-aac4-68f922fe40c2',
+    'description': 'Frankfurt - is a beautiful city',
+    'name': 'Frankfurt',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/4.jpg',
+        'description': 'Frankfurt with an embankment of a mighty river as a centre of attraction'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/16.jpg',
+        'description': 'Frankfurt famous for its crowded street markets with the best street food in Asia'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/10.jpg',
+        'description': 'Frankfurt middle-eastern paradise'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/1.jpg',
+        'description': 'Frankfurt for those who value comfort and coziness'
+      }
+    ]
+  },
+  {
+    'id': '62a6a836-611b-4c0a-a76c-e441b89f1109',
+    'description': 'Paris - is a beautiful city',
+    'name': 'Paris',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/17.jpg',
+        'description': 'Paris a perfect place to stay with a family'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/3.jpg',
+        'description': 'Paris middle-eastern paradise'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/7.jpg',
+        'description': 'Paris with an embankment of a mighty river as a centre of attraction'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/18.jpg',
+        'description': 'Paris is a beautiful city'
+      }
+    ]
+  },
+  {
+    'id': '2db689a7-75cd-41bb-a0cf-09de8e28560d',
+    'description': '',
+    'name': 'Madrid',
+    'pictures': []
+  },
+  {
+    'id': '69e64a90-0eab-4463-b1bb-57d13224b5b9',
+    'description': 'Den Haag - with a beautiful old town',
+    'name': 'Den Haag',
+    'pictures': [
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/12.jpg',
+        'description': 'Den Haag famous for its crowded street markets with the best street food in Asia'
+      },
+      {
+        'src': 'https://24.objects.htmlacademy.pro/static/destinations/12.jpg',
+        'description': 'Den Haag with a beautiful old town'
+      }
+    ]
+  }
+];
