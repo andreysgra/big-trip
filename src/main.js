4ebce07fd0d0d5c4bf231ed072ec8cb51d2d99f2
Andrey Grachev <andreysgra@gmail.com>
2026-04-28 18:59:20 +0300

    Реализует отображение данных в представлении точки маршрута.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import TripFiltersPresenter from './presenter/trip-filters-presenter';
 import TripEventsPresenter from './presenter/trip-events-presenter';
 import {points} from './mocks/points';
 import PointsModel from './model/points-model';
+import {destinations} from './mocks/destinations';
+import DestinationsModel from './model/destinations-model';
+import {offers} from './mocks/offers';
+import OffersModel from './model/offers-model';
 
 const bodyElement = document.body;
 const tripMainElement = bodyElement.querySelector('.trip-main');
@@ -10,10 +14,13 @@ const tripControlsFiltersElement = bodyElement.querySelector('.trip-controls__fi
 const tripEventsElement = bodyElement.querySelector('.trip-events');
 
 const pointsModel = new PointsModel(points);
+const destinationsModel = new DestinationsModel(destinations);
+const offersModel = new OffersModel(offers);
 
 const tripInfoPresenter = new TripInfoPresenter(tripMainElement);
 const tripFiltersPresenter = new TripFiltersPresenter(tripControlsFiltersElement);
-const tripEventsPresenter = new TripEventsPresenter(tripEventsElement, pointsModel);
+const tripEventsPresenter =
+  new TripEventsPresenter(tripEventsElement, pointsModel, destinationsModel, offersModel);
 
 tripInfoPresenter.init();
 tripFiltersPresenter.init();
--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -3,28 +3,36 @@ import {render} from '../framework/render';
 import TripEventsListView from '../view/trip-events-list-view';
 import TripEventsItemView from '../view/trip-events-item-view';
 import TripEventFormView from '../view/trip-event-form-view';
+import {sortPointsByDate} from '../utils/point';
 
 export default class TripEventsPresenter {
   #container = null;
   #pointsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
   #points = [];
   #tripSorComponent = new TripSortView;
   #tripEventsListComponent = new TripEventsListView();
 
-  constructor(container, pointsModel) {
+  constructor(container, pointsModel, destinationsModel, offersModel) {
     this.#container = container;
     this.#pointsModel = pointsModel;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
   }
 
   init() {
-    this.#points = [...this.#pointsModel.points];
+    this.#points = [...this.#pointsModel.points].sort(sortPointsByDate);
 
     render(this.#tripSorComponent, this.#container);
     render(this.#tripEventsListComponent, this.#container);
     render(new TripEventFormView, this.#tripEventsListComponent.element);
 
-    for (let i = 0; i < 3; i++) {
-      render(new TripEventsItemView(), this.#tripEventsListComponent.element);
-    }
+    this.#points.forEach((point) => {
+      const destination = this.#destinationsModel.getDestination(point.destination);
+      const offers = this.#offersModel.getOffers(point.type);
+
+      render(new TripEventsItemView(point, destination, offers), this.#tripEventsListComponent.element);
+    });
   }
 }
