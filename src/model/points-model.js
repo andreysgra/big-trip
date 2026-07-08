406f5727396c3906a1f210f84a4e8534785d9de9
Andrey Grachev <andreysgra@gmail.com>
2026-07-08 15:30:06 +0300

    Добавляет в модель точек маршрута возможность удаления точки.

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -13,6 +13,21 @@ export default class PointsModel extends Observable {
     return this.#points;
   }
 
+  delete(updateType, update) {
+    const index = this.#points.findIndex((comment) => comment.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete nonexistent point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      ...this.#points.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
+
   update(updateType, update) {
     const index = this.#points.findIndex((point) => point.id === update.id);
 
