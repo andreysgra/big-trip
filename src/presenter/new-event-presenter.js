5beb79241e98fc9d53e542254270ef8590060d0b
Andrey Grachev <andreysgra@gmail.com>
2026-07-08 23:35:42 +0300

    Создаёт презентер новой точки маршрута.

---
+++ src/presenter/new-event-presenter.js
@@ -0,0 +1,71 @@
+import TripEventFormView from '../view/trip-event-form-view';
+import {BLANK_POINT, UpdateType, UserAction} from '../const';
+import {remove, render, RenderPosition} from '../framework/render';
+
+export default class NewEventPresenter {
+  #container = null;
+
+  #destinationsModel = null;
+  #offersModel = null;
+
+  #handleDataChange = () => null;
+  #handleDestroy = () => null;
+
+  #tripEventFormComponent = null;
+
+  constructor({container, destinationsModel, offersModel, onDataChange, onDestroy}) {
+    this.#container = container;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+    this.#handleDataChange = onDataChange;
+    this.#handleDestroy = onDestroy;
+  }
+
+  destroy() {
+    if (this.#tripEventFormComponent === null) {
+      return;
+    }
+
+    this.#handleDestroy();
+
+    remove(this.#tripEventFormComponent);
+    this.#tripEventFormComponent = null;
+  }
+
+  init() {
+    if (this.#tripEventFormComponent !== null) {
+      return;
+    }
+
+    this.#tripEventFormComponent = new TripEventFormView({
+      point: {
+        ...BLANK_POINT,
+        destination: this.#destinationsModel.getDefaultDestinationId()
+      },
+      destinations: this.#destinationsModel.destinations,
+      offers: this.#offersModel.offers,
+      onRollupClick: () => null,
+      onFormSubmit: this.#formSubmitHandler,
+      onDeleteButtonClick: this.#deleteButtonClickHandler
+    });
+
+    render(this.#tripEventFormComponent, this.#container, RenderPosition.AFTERBEGIN);
+  }
+
+  #deleteButtonClickHandler = () => {
+    this.destroy();
+  };
+
+  #formSubmitHandler = (point) => {
+    this.#handleDataChange(
+      UserAction.ADD_POINT,
+      UpdateType.MINOR,
+      {
+        id: crypto.randomUUID(),
+        ...point
+      }
+    );
+
+    this.destroy();
+  };
+}
