9ef2725d3db5d905bb77dc86a7b280c245caa7f8
Andrey Grachev <andreysgra@gmail.com>
2026-07-09 23:11:39 +0300

    Создаёт модель фильтров.

---
+++ src/model/filter-model.js
@@ -0,0 +1,16 @@
+import Observable from '../framework/observable';
+import {FilterType} from '../const';
+
+export default class FilterModel extends Observable {
+  #filter = FilterType.EVERYTHING;
+
+  getFilter() {
+    return this.#filter;
+  }
+
+  setFilter(updateType, update) {
+    this.#filter = update;
+
+    this._notify(updateType, update);
+  }
+}
