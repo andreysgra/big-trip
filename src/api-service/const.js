30b79ac79d031527f470b80f58528861d06804ab
Andrey Grachev <andreysgra@gmail.com>
2026-07-13 14:30:40 +0300

    Создаёт класс для работы с API точек маршрута.

---
+++ src/api-service/const.js
@@ -0,0 +1,4 @@
+export const Method = {
+  GET: 'GET',
+  PUT: 'PUT'
+};
---
+++ src/api-service/points-api-service.js
@@ -0,0 +1,21 @@
+import ApiService from '../framework/api-service';
+import {Method} from './const';
+
+export default class PointsApiService extends ApiService {
+  get points() {
+    return this._load({url: 'points'})
+      .then(ApiService.parseResponse);
+  }
+
+  updatePoint = async (point) => {
+    const response = await this._load({
+      url: `points/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(point),
+      headers: new Headers({'Content-Type': 'application/json'})
+    });
+
+    return await ApiService.parseResponse(response);
+  };
+}
+
