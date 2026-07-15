423f052d7a203571cfa1b6b8608959568fba884b
Andrey Grachev <andreysgra@gmail.com>
2026-07-15 19:03:40 +0300

    Реализует обновление данных точки маршрута на сервере.

--- src/api-service/points-api-service.js
+++ src/api-service/points-api-service.js
@@ -11,11 +11,27 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'})
     });
 
     return await ApiService.parseResponse(response);
   };
-}
 
+  #adaptToServer(point) {
+    const adaptedPoint = {
+      ...point,
+      ['base_price']: point.basePrice,
+      ['date_from']: point.dateFrom,
+      ['date_to']: point.dateTo,
+      ['is_favorite']: point.isFavorite
+    };
+
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
+  }
+}
